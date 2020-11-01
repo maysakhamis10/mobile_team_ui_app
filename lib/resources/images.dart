9ea31863d3ed9eb2c074802e05d063e00345b058
Hadeer <mhadeer95@yahoo.com>
2020-11-01 12:21:08 +0200

    home page finished

---
+++ lib/resources/images.dart
@@ -0,0 +1,16 @@
+const String imagePath = "assets/images/";
+
+const String eCommerceAppImagePath = imagePath+"e_commerce_app/";
+const String designCourseAppImagePath = imagePath+"design_course/";
+const String fitnessAppImagePath = imagePath+"fitness_app/";
+const String foodAppImagePath = imagePath+"food_app/";
+const String hotelAppImagePath = imagePath+"hotel/";
+
+
+
+const String eCommerceAppHome = eCommerceAppImagePath+"e_commerce_app_home.PNG";
+const String designCourseAppHome = designCourseAppImagePath+"design_course_app_home.png";
+const String fitnessAppHome = fitnessAppImagePath+"fitness_app_home.png";
+const String foodAppHome = foodAppImagePath+"food_app_home.PNG";
+const String hotelAppHome = hotelAppImagePath+"hotel_app_home.png";
+
